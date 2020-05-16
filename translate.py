08e746db79b2b3865868e4c8ef2e9fc1d6654eae
Marcin Plisz <plisz.marcin@gmail.com>
2020-05-16 14:58:12 +0000

    adding translate example

---
+++ translate.py
@@ -0,0 +1,6 @@
+import boto3
+
+client = boto3.client("translate", region_name = "us-east-1")
+text = "Hola mi nombre es Marcin"
+result = client.translate_text(Text=text, SourceLanguageCode = "auto", TargetLanguageCode = "en")
+print(result["TranslatedText"])
