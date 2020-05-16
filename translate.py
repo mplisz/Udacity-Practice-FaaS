525023b8d7aa648570718371ec9e21613798d906
Marcin Plisz <plisz.marcin@gmail.com>
2020-05-16 15:49:59 +0000

    Commit all files from AWS

--- translate.py
+++ translate.py
@@ -3,4 +3,4 @@ import boto3
 client = boto3.client("translate", region_name = "us-east-1")
 text = "Hola mi nombre es Marcin"
 result = client.translate_text(Text=text, SourceLanguageCode = "auto", TargetLanguageCode = "en")
-print(result["TranslatedText"])
+print(result["TranslatedT"])
